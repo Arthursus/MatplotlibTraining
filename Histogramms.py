2f01bdd0691f207c8a40ae3fd8962cff818ce7e0
Arthur Chaikovski <arthur.chaykovski@gmail.com>
2021-09-26 00:44:04 +0300

    Add bars diagrams

--- Histogramms.py
+++ Histogramms.py
@@ -1,9 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-fig1 = plt.figure(figsize=(6, 4))
-ax1 = fig1.add_subplot()
 y = np.random.normal(0, 2, 500)
-ax1.hist(y, 50)
-ax1.grid()
+x = np.linspace(np.min(y), np.max(y), 10)
+bars = [len(y[np.bitwise_and(y >= x[i], y < x[i + 1])]) for i in range(len(x) - 1)]
+values = x + np.append(np.diff(x), 0) / 2  # Bringing to fitting size of bars
+
+fig1 = plt.figure(figsize=(6, 4))
+ax = fig1.add_subplot()
+ax.bar(values[:-1], bars)
+fig1.show()
+
+# ax.hist(y, 50)
+
+fig2 = plt.figure(figsize=(6, 4))
+ax = fig2.add_subplot()
+ax.barh(values[:-1], bars)
+fig2.show()
+
 plt.show()
