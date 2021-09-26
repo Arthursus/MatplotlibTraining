d693938e04192f05ba69b9a18764315e84d41ef7
Arthur Chaikovski <arthur.chaykovski@gmail.com>
2021-09-26 23:53:56 +0300

    Change animations using OOP

--- Animations.py
+++ Animations.py
@@ -2,14 +2,18 @@ import time
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.arange(-2*np.pi, 2*np.pi, 0.1)
-
 plt.ion()
+
+fig, ax = plt.subplots()
+
+x = np.arange(-2*np.pi, 2*np.pi, 0.1)
+y = np.cos(x)
+line, = ax.plot(x, y)
+
 for delay in np.arange(-2*np.pi, 2*np.pi, 0.1):
     y = np.cos(x + delay)
 
-    plt.clf()
-    plt.plot(x, y)
+    line.set_ydata(y)
 
     plt.draw()
     plt.gcf().canvas.flush_events()
