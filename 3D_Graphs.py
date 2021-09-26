2fd95053fb27a73e7b9f1b9d1ad81e786c3aca88
Arthur Chaikovski <arthur.chaykovski@gmail.com>
2021-09-26 14:20:02 +0300

    Add flat graph

--- 3D_Graphs.py
+++ 3D_Graphs.py
@@ -5,4 +5,13 @@ from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure(figsize=(7, 4))
 # ax_3d = Axes3D(fig) # deprecated
 ax_3d = fig.add_subplot(projection='3d')
+
+x = np.linspace(0, 10, 50)
+z = np.cos(x)
+
+ax_3d.plot(x, x, z)
+ax_3d.set_xlabel('x')
+ax_3d.set_ylabel('y')
+ax_3d.set_zlabel('z')
+
 plt.show()
