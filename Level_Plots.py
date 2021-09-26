81d64d04a26d08830362dde70ca7c372cc56f655
Arthur Chaikovski <arthur.chaykovski@gmail.com>
2021-09-26 23:16:14 +0300

    Add 2D level plots

--- Level_Plots.py
+++ Level_Plots.py
@@ -1,8 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-fig = plt.figure(figsize=(7, 4))
-ax_3d = fig.add_subplot(projection='3d')
+# fig = plt.figure(figsize=(7, 4))
+# ax_3d = fig.add_subplot(projection='3d')
+
+fig, ax = plt.subplots(1, 2)
+fig.set_size_inches(10, 5)
 
 x = np.arange(-2 * np.pi, 2 * np.pi, 0.2)
 y = np.arange(-2 * np.pi, 2 * np.pi, 0.2)
@@ -10,6 +13,11 @@ xgrid, ygrid = np.meshgrid(x, y)
 zgrid = np.sin(xgrid) * np.sin(ygrid) / (xgrid * ygrid)
 
 # ax_3d.contour(xgrid, ygrid, zgrid)  # 3D plot in the form of level lines
-ax_3d.contourf(xgrid, ygrid, zgrid)  # 3D plot in the form of level surfaces
+# ax_3d.contourf(xgrid, ygrid, zgrid)  # 3D plot in the form of level surfaces
+
+c0 = ax[0].contour(xgrid, ygrid, zgrid, [-0.5, -0.2, 0, 0.2, 0.5, 0.7], colors=['g', 'b', 'r'])  # 2D plot in the form of level lines
+c1 = ax[1].contourf(xgrid, ygrid, zgrid, 15, cmap='Reds')  # 2D plot in the form of level surfaces
+
+c0.clabel(colors='k', fmt='%.2f')  # Analog of ax[0].clabel(c0)
 
 plt.show()
