9164de98e605d68d4248e9809702d82d0776cbd3
Arthur Chaikovski <arthur.chaykovski@gmail.com>
2021-09-27 00:18:42 +0300

    Add animation with class FuncAnimation

--- Animations.py
+++ Animations.py
@@ -1,8 +1,9 @@
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation, ArtistAnimation
 
-plt.ion()
+# plt.ion()
 
 fig, ax = plt.subplots()
 
@@ -10,15 +11,36 @@ x = np.arange(-2*np.pi, 2*np.pi, 0.1)
 y = np.cos(x)
 line, = ax.plot(x, y)
 
-for delay in np.arange(-2*np.pi, 2*np.pi, 0.1):
-    y = np.cos(x + delay)
+# for delay in np.arange(-2*np.pi, 2*np.pi, 0.1):
+#     y = np.cos(x + delay)
+#
+#     line.set_ydata(y)
+#
+#     plt.draw()
+#     plt.gcf().canvas.flush_events()
+#
+#     time.sleep(0.02)
 
-    line.set_ydata(y)
 
-    plt.draw()
-    plt.gcf().canvas.flush_events()
+def update_cos(frame, line0, x0):
+    # frame - parameter changing from frame to frame
+    # in this case this is an initial phase (degree)
+    # line0 - link to Line2D object
+    line0.set_ydata(np.cos(x0 + frame))
+    return [line0]
 
-    time.sleep(0.02)
 
-plt.ioff()
+phasa = np.arange(0, 4*np.pi, 0.1)
+
+animation = FuncAnimation(
+    fig,                # Figure displaying animation
+    func=update_cos,    # Updating current frame function
+    frames=phasa,       # Parameter changing from frame to frame
+    fargs=(line, x),    # Additional parameters for update_cos function
+    interval=30,        # Delay between frames (ms)
+    blit=True,          # Double buffering
+    repeat=False        # Cycling of animation
+)
+
+# plt.ioff()
 plt.show()
