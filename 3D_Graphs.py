b5961346a9fd8335cd3bea099c1b7a6d5468743a
Arthur Chaikovski <arthur.chaykovski@gmail.com>
2021-09-26 14:38:07 +0300

    Add volumetric graphs

--- 3D_Graphs.py
+++ 3D_Graphs.py
@@ -1,15 +1,25 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from mpl_toolkits.mplot3d import Axes3D
 
 fig = plt.figure(figsize=(7, 4))
 # ax_3d = Axes3D(fig) # deprecated
 ax_3d = fig.add_subplot(projection='3d')
 
-x = np.linspace(0, 10, 50)
-z = np.cos(x)
+# x = np.linspace(0, 10, 50)
+# z = np.cos(x)
+#
+# ax_3d.plot(x, x, z)
+# ax_3d.set_xlabel('x')
+# ax_3d.set_ylabel('y')
+# ax_3d.set_zlabel('z')
 
-ax_3d.plot(x, x, z)
+x = np.arange(-2 * np.pi, 2 * np.pi, 0.2)
+y = np.arange(-2 * np.pi, 2 * np.pi, 0.2)
+xgrid, ygrid = np.meshgrid(x, y)
+zgrid = np.sin(xgrid) * np.sin(ygrid) / (xgrid * ygrid)
+
+# ax_3d.plot_wireframe(xgrid, ygrid, zgrid)
+ax_3d.plot_surface(xgrid, ygrid, zgrid, rstride=1, cstride=5, cmap='plasma')
 ax_3d.set_xlabel('x')
 ax_3d.set_ylabel('y')
 ax_3d.set_zlabel('z')
